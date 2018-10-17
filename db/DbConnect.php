94d62612e71ca3147d4ab26571de992c4aecaefb
Sasa Popovic <sasa.popovic@devtechgroup.com>
2018-10-17 16:18:56 +0200

    home screen and validation added

---
+++ db/DbConnect.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sasa.popovic
+ * Date: 10/17/2018
+ * Time: 4:02 PM
+ */
+
+namespace App\db;
+
+$serverName = 'localhost';
+$username = 'root';
+$password = '';
+$dbName = 'testapp';
+
+$conn = mysqli_connect($serverName, $username, $password, $dbName);
+
+if(!$conn){
+	echo 'Connection Error ' . mysqli_connect_error();
+}
--- home.php
+++ home.php
@@ -5,3 +5,24 @@
  * Date: 10/17/2018
  * Time: 3:51 PM
  */
+namespace App;
+session_start();
+if(!isset($_SESSION['username'])){
+	header('location:index.php');
+}
+?>
+
+<html>
+<head>
+	<title>Login and Register</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+	<a href="logout.php">Logout</a>
+	<div style="width: 500px; margin: 50px auto;">
+		<h3>Welcome <?php echo $_SESSION['username']; ?></h3
+	</div>
+</div>
+</body>
+</html>
